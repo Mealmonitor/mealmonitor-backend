f2e318feae27cbbca6efdd48e5a8750c3ad7b53a
Andrei Galitianu <andrei.galitianu@ciconia-systems.com>
2023-10-18 23:43:31 +0300

    Initial commit

---
+++ src/main/java/com/galitianu/mealmonitorbackend/MealmonitorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.galitianu.mealmonitorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealmonitorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealmonitorBackendApplication.class, args);
+	}
+
+}
